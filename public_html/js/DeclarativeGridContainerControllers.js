3807f5b931b6a31eb1f59ccd73f4fd49b95b46ac
GB001894 <GB001894@PONENTE>
2015-02-06 11:01:55 +0000

    Merge origin/master
    Conflicts:
    	public_html/BOMGridExample1.html
    	public_html/js/BOMGridControllers.js

--- public_html/js/DeclarativeGridContainerControllers.js
+++ public_html/js/DeclarativeGridContainerControllers.js
@@ -6,7 +6,7 @@
 angular.module("declarativeGridContainer.tsiotsias.uk")
     .controller("DeclarativeGridController", ['$rootScope','$scope', '$element', '$attrs', 'getHTTPDataService',
         function($rootScope, $scope, $element, $attrs, getHTTPDataService) {
-            var httpDataPromise = getHTTPDataService.getData("ProductSummary.json");
+            var httpDataPromise = getHTTPDataService.getData("ProductsGridDescriptor.json");
             // show the spinning wheel ....
             $('#loading').show();
             // Now wait until the promise is fulfilled
@@ -19,89 +19,18 @@ angular.module("declarativeGridContainer.tsiotsias.uk")
         function initialiseGrid(gridData){
             var gridElement = $element[0];
             var gridContainerElement = $(gridElement).parent()[0];
+            // now bypass the Height attribute passed in via th definition
             var gridContainerHeight = $(gridElement).parent().height();
-            var pageAble = { refresh: true, pageSizes: [5, 10, 15, 20, 25], input: false  };
-            $(gridElement).kendoGrid({
-                dataSource: {
-                    data: gridData,
-                    pageSize: 15
-                },
-                toolbar: [
-                    "create", "cancel", "save"
-                ],
-                columns: [
-                {
-                    field: "Part_Number",
-                    title: "Item Number",
-                    width: 120
-                }, 
-                {
-                    field: "Part_Name",
-                    title: "Item Name",
-                    filterable: false
-                }, 
-                {
-                    field: "Revision",
-                    title: "Version",
-                    width: 80,
-                    filterable: true
-                },  
-                {
-                    field: "Phase",
-                    title: "Status",
-                    width: 130,
-                    filterable: true
-                },
-                {
-                    field: "Description",
-                    title: "Item Notes",
-                    filterable: false
-                },
-                {   
-                    command: [{
-                        name: "edit",
-                        text: { 
-                            edit: "Edit",               // This is the localization for Edit button
-                            update: "Save",             // This is the localization for Update button
-                            cancel: "Cancel changes"    // This is the localization for Cancel button
-                        }
-                    },
-                    { 
-                        name: "destroy", 
-                        text: "Delete Office"           // This is the localization for Delete button
-                    }
-                ],
-                    title: "&nbsp;",
-                    width: "300px"
-                }],
-                autoBind: true,
-                batch: false,
-                columnMenu: true,
-                selectable: "multiple, row",
-                groupable: false,
-                scrollable: true,
-                sortable: true,
-                pageable: pageAble,
-                resizable: true,
-                filterable: true,
-                editable: {
-                    mode: "popup"
-                },
-                height: gridContainerHeight
-            });
+            gridData.height = gridContainerHeight;
+            $(gridElement).kendoGrid(gridData);
             var grid = $(gridElement).data("kendoGrid");
+            console.log("Grid is : "+grid);
             // deal with pager change events
             var gridPager = grid.pager;
             gridPager.bind("change", pager_change);
             // deal with data source change events
             var gridDataSource = grid.dataSource;
             gridDataSource.bind("change", dataSource_change);
-            // deal with saveChanges events
-            grid.bind("saveChanges", grid_saveChanges);
-            // deal with edit events
-            grid.bind("edit", grid_edit);
-            // deal with dataBinding events
-            grid.bind("dataBind", grid_dataBind);
             var dataArea = $(gridElement).find(".k-grid-content");
             var gridDecorationsHeight = gridContainerElement.clientHeight - dataArea.height();
             console.log("<-- Initialisation --->");
@@ -133,27 +62,6 @@ angular.module("declarativeGridContainer.tsiotsias.uk")
             function dataSource_change() {
                 console.log("dataSource change event");
                 resizeGridToFitContainer();
-            }
-            //
-            // manage the saveChanges event
-            function grid_saveChanges(e) {
-                if (!confirm("Are you sure you want to save all changes?")) {
-                    e.preventDefault();
-                    alert("Changes Discarded !");
-                }
-                else {
-                    alert("Changes Saved !");
-                }
-            }
-            //
-            // manage the edit event on a row
-            function grid_edit(e) {
-                alert("Grid Edit");
-            }
-            //
-            // manage the databinding event on a row
-            function grid_dataBind (e) {
-                alert("Grid Data Bind");
             }
         }  
     }]);
