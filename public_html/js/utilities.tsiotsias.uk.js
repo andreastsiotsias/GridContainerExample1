6ebf1c1315139b47e0256392402d3108ab653e8e
GB001894 <GB001894@PONENTE>
2015-02-26 16:01:09 +0000

    Merge origin/master
    Conflicts:
    	public_html/BOMGridExample1.html
    	public_html/js/BOMGridControllers.js

--- public_html/js/utilities.tsiotsias.uk.js
+++ public_html/js/utilities.tsiotsias.uk.js
@@ -21,6 +21,15 @@ angular.module('utilities.tsiotsias.uk', [])
                     console.log("Object key : "+keys[i]+" value : "+obj[keys[i]]);
                 }
                 console.log("------->");
+            },
+            guid: function () {
+                function s4() {
+                    return Math.floor((1 + Math.random()) * 0x10000)
+                        .toString(16)
+                        .substring(1);
+                }
+                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                    s4() + '-' + s4() + s4() + s4();
             }
         };
     });
